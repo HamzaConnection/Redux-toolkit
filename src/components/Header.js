82809d3cddca519a7f6fb974e5090025dfe98c03
Hamza Laroussi <hamza.laroussi@capgemini.com>
2022-11-07 13:28:20 +0100

    done

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import classes from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux'
+import { authActions } from '../store/auth';
+
+
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = (e) => {
+    e.preventDefault()
+    dispatch(authActions.logout())
+  }
+
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isAuth &&
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={loginHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      }
+
+    </header>
+  );
+};
+
+export default Header;
